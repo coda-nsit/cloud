0d6da7200232b91564e2d4dba6fa793ea5995614
manavbagai <manavbagai@gmail.com>
2019-05-02 09:14:05 -0700

    db config

--- cloud/cloud/settings.py
+++ cloud/cloud/settings.py
@@ -74,13 +74,46 @@ WSGI_APPLICATION = 'cloud.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/cloudproject2-239105:us-central1:ccphase2',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'NAME': 'ManavPythonCC',
+        }
+    }
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': '[YOUR-DATABASE]',
+            'USER': '[YOUR-USERNAME]',
+            'PASSWORD': '[YOUR-PASSWORD]',
+        }
+    }
+# [END db_setup]
+
+
 
 
 # Password validation
